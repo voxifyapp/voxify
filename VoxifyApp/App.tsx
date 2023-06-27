e531c62d3f390f48ab88014250c147e709ac016d
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-06-27 21:18:54 +0530

    Finish react navigation setup

--- VoxifyApp/App.tsx
+++ VoxifyApp/App.tsx
@@ -1,16 +1,14 @@
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {SafeAreaView, ScrollView, StatusBar, Text, View} from 'react-native';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View>
-          <Text>Hello World!</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
