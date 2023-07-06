8300f0d56db55c7e20738f175698a396fe96deef
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-07-06 17:51:34 +0530

    fix tamgui

--- VoxifyApp/App.tsx
+++ VoxifyApp/App.tsx
@@ -1,17 +1,24 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { View } from 'react-native';
-import { H1, TamaguiProvider, Theme } from 'tamagui';
+import { TamaguiProvider, Theme } from 'tamagui';
+import { Login } from './modules/auth/screens/Login';
 import tamaguiConfig from './tamagui.config';
+
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
     <TamaguiProvider config={tamaguiConfig}>
-      <Theme name="light">
+      <Theme name="dark">
         <NavigationContainer>
-          <View>
-            <H1>Hello World</H1>
-          </View>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="Login"
+              component={Login}
+            />
+          </Stack.Navigator>
         </NavigationContainer>
       </Theme>
     </TamaguiProvider>
--- VoxifyApp/modules/auth/screens/Login.tsx
+++ VoxifyApp/modules/auth/screens/Login.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { H1, YStack } from 'tamagui';
+import { Button, H1, YStack } from 'tamagui';
 
 export const Login = () => {
   return (
     <YStack>
       <H1>Hello World 2!</H1>
+      <Button theme="green">Hello World</Button>
     </YStack>
   );
 };
