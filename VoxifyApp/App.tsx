a2213c254e21ef31b327cf5117797bf92009fe0a
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-07-07 10:40:50 +0530

    add google login

--- VoxifyApp/App.tsx
+++ VoxifyApp/App.tsx
@@ -1,13 +1,22 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { TamaguiProvider, Theme } from 'tamagui';
 import { Login } from './modules/auth/screens/Login';
 import tamaguiConfig from './tamagui.config';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
+  useEffect(() => {
+    //TODO Change this
+    GoogleSignin.configure({
+      webClientId:
+        '44840556270-pitj1tnp57rddm5bnbib0a4ni8fn5gth.apps.googleusercontent.com',
+    });
+  }, []);
+
   return (
     <TamaguiProvider config={tamaguiConfig}>
       <Theme name="dark">
--- VoxifyApp/modules/auth/screens/Login.tsx
+++ VoxifyApp/modules/auth/screens/Login.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import React, { useEffect } from 'react';
 import { Button, H1, YStack } from 'tamagui';
 
 export const Login = () => {
+  useEffect(() => {}, []);
+
   return (
     <YStack>
       <H1>Hello World 2!</H1>
-      <Button theme="green">Hello World</Button>
+      <Button
+        onPress={async () => {
+          const userInfo = await GoogleSignin.signIn();
+          console.log(userInfo);
+        }}
+        theme="green">
+        Hello World
+      </Button>
     </YStack>
   );
 };
