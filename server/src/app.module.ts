63af288d24aa5b5e572594544349a2636ff9656e
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-07-19 13:09:35 +0530

    trim unwanted files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
@@ -15,8 +13,6 @@ import { AuthModule } from './auth/auth.module';
     }),
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}
