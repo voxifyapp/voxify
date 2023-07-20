2c38ecd5b918507cf34cdcb6a564eb073148f77f
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-07-20 12:56:25 +0530

    rewrite tests using mocks

--- server/src/auth/firebase/firebase.service.spec.ts
+++ server/src/auth/firebase/firebase.service.spec.ts
@@ -1,41 +1,68 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { FirebaseService } from './firebase.service';
-import {
-  createTestFirebaseUser,
-  createIdTokenForFirebaseUser,
-} from 'test/common/firebase';
+
+jest.mock('firebase-admin', () => {
+  return {
+    apps: [],
+    initializeApp: jest.fn().mockReturnValue({
+      auth: jest.fn().mockReturnValue({
+        verifyIdToken: jest.fn(),
+      }),
+    }),
+    app: jest.fn().mockReturnValue({
+      auth: jest.fn().mockReturnValue({
+        verifyIdToken: jest.fn(),
+      }),
+    }),
+    credential: {
+      cert: jest.fn(),
+    },
+  };
+});
 
 describe('FirebaseService', () => {
   let service: FirebaseService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef = await Test.createTestingModule({
       providers: [FirebaseService],
     }).compile();
 
-    service = module.get<FirebaseService>(FirebaseService);
+    service = moduleRef.get<FirebaseService>(FirebaseService);
   });
 
-  it('should be able to verify a valid idtoken', async () => {
-    const firebaseUser = await createTestFirebaseUser(service.admin);
-    const idToken = await createIdTokenForFirebaseUser(
-      service.admin,
-      firebaseUser.uid,
-    );
+  it('should verify ID token if valid and return user', async () => {
+    expect(true).toBe(true);
+    const idToken = 'valid_token';
+    const mockUser = { uid: 'test_uid' };
+
+    service.admin.auth().verifyIdToken = jest
+      .fn()
+      .mockReturnValueOnce(mockUser);
 
     const result = await service.getFirebaseUserFromIdToken(idToken);
-
-    expect(result.uid).toEqual(firebaseUser.uid);
+    expect(result.uid).toEqual(mockUser.uid);
+    expect(service.admin.auth().verifyIdToken).toHaveBeenCalledWith(idToken);
   });
 
   it('should throw an error if idtoken is invalid', async () => {
+    const invalidToken = 'fakeIdTokenThatDoesNotExist';
+    service.admin.auth().verifyIdToken = jest
+      .fn()
+      .mockImplementationOnce(async () => {
+        throw new Error();
+      });
+
     let error;
     try {
-      await service.getFirebaseUserFromIdToken('fakeIdTokenThatDoesNotExist');
+      await service.getFirebaseUserFromIdToken(invalidToken);
     } catch (err) {
       error = err;
     }
 
     expect(error).not.toBeNull();
+    expect(service.admin.auth().verifyIdToken).toHaveBeenCalledWith(
+      invalidToken,
+    );
   });
 });
