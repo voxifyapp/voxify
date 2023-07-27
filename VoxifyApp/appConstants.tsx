f3a78eb28a8df20353d4d6d1787520c04f1ab1f2
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-07-27 12:39:08 +0530

    setup rn config

--- VoxifyApp/appConstants.tsx
+++ VoxifyApp/appConstants.tsx
@@ -1,3 +1,5 @@
+import Config from 'react-native-config';
+
 export class Constants {
-  static readonly VOXIFY_API_ENDPOINT = process.env.VOXIFY_ENDPOINT;
+  static readonly VOXIFY_API_ENDPOINT = Config.VOXIFY_API_ENDPOINT;
 }
