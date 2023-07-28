8c46a2d395ee9ed56c80c0122483ce5da071115f
sahmed02 <saif.sabeer.ahmed@yahooinc.com>
2023-07-27 20:52:47 -0700

    Adding mutation to set proficiency and invalidating query that fetched the profile

--- VoxifyApp/api/auth/profile.ts
+++ VoxifyApp/api/auth/profile.ts
@@ -1,7 +1,17 @@
 import { authAxios } from '@voxify/axiosClient';
-import { ProfileEntity } from '@voxify/types/auth/profile';
+import { ProficiencyLevel, ProfileEntity } from '@voxify/types/auth/profile';
 
 export const FETCH_OR_CREATE_PROFILE_QUERY = 'FETCH_OR_CREATE_PROFILE_QUERY';
 export const fetchOrCreateProfile = async (): Promise<ProfileEntity> => {
   return (await authAxios.post('/profile')).data;
+};
+
+export const setProficiency = async (
+  proficiency: ProficiencyLevel,
+): Promise<ProfileEntity> => {
+  return (
+    await authAxios.post('/set-proficiency-level', {
+      proficiencyLevel: proficiency,
+    })
+  ).data;
 };
--- VoxifyApp/modules/auth/screens/ProfileSetup/ProfileSetup.tsx
+++ VoxifyApp/modules/auth/screens/ProfileSetup/ProfileSetup.tsx
@@ -1,50 +1,74 @@
+import {
+  FETCH_OR_CREATE_PROFILE_QUERY,
+  setProficiency,
+} from '@voxify/api/auth/profile';
 import { useAppContext } from '@voxify/context/AppContext';
 import {
   ProfileCompletionStep,
   useGetCurrentProfileStep,
 } from '@voxify/hooks/profile';
+import { ProficiencyLevel } from '@voxify/types/auth/profile';
 import React from 'react';
-import { H1, YStack } from 'tamagui';
+import { useMutation, useQueryClient } from 'react-query';
+import { Button, H1, View, YStack } from 'tamagui';
 
 export const ProfileSetup = () => {
   const { profile } = useAppContext();
+  const queryClient = useQueryClient();
   const currentProfileStep = useGetCurrentProfileStep(
     profile,
   ) as ProfileCompletionStep;
+
+  const { mutate } = useMutation(
+    (proficiency: ProficiencyLevel) => {
+      return setProficiency(proficiency);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [FETCH_OR_CREATE_PROFILE_QUERY],
+        });
+      },
+    },
+  );
+
+  function selectProficiencyHandler(proficiency: ProficiencyLevel) {
+    // Make HTTP POST call to set Proficiency
+    mutate(proficiency);
+  }
+
   return (
     <YStack>
       {currentProfileStep === ProfileCompletionStep.SELECT_PROFICIENCY ? (
-        <H1>Select Proficiency!</H1>
+        <View>
+          <Button
+            onPress={selectProficiencyHandler.bind(
+              this,
+              ProficiencyLevel.BEGINNER,
+            )}
+            theme={'green'}>
+            Beginner
+          </Button>
+          <Button
+            onPress={selectProficiencyHandler.bind(
+              this,
+              ProficiencyLevel.MEDIUM,
+            )}
+            theme={'green'}>
+            Medium
+          </Button>
+          <Button
+            onPress={selectProficiencyHandler.bind(
+              this,
+              ProficiencyLevel.ADVANCED,
+            )}
+            theme={'green'}>
+            Advanced
+          </Button>
+        </View>
       ) : currentProfileStep === ProfileCompletionStep.SELECT_MEMBERSHIP ? (
         <H1>SELECT_MEMBERSHIP!</H1>
       ) : null}
     </YStack>
   );
 };
-
-// const { mutate, isLoading, error, reset } = useMutation(
-//   ({
-//     resmanAccountId,
-//     resmanApiKey,
-//     resmanPartnerId,
-//   }: {
-//     resmanApiKey: string;
-//     resmanPartnerId: string;
-//     resmanAccountId: string;
-//   }) => {
-//     return saveSettings({ resmanApiKey, resmanAccountId, resmanPartnerId });
-//   },
-//   {
-//     onSuccess: () => {
-//       notifications.show({
-//         title: "Resman integration",
-//         message:
-//           "Resman details have been updated successfully! You may need to wait for a few minutes before you can see the renter data.",
-//         color: "green",
-//         icon: <IconCheck color={theme.colors.green[0]} />,
-//       });
-//       queryClient.invalidateQueries({ queryKey: [QUERY_SETTINGS] });
-//       onClose();
-//     },
-//   }
-// );
