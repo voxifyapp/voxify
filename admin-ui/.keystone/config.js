671d9cca5264db120515045d363092c54ddd71f9
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-08-04 18:11:17 +0530

    create course unit mapping

--- admin-ui/.keystone/config.js
+++ admin-ui/.keystone/config.js
@@ -29,7 +29,6 @@ var import_core2 = require("@keystone-6/core");
 var import_core = require("@keystone-6/core");
 var import_access = require("@keystone-6/core/access");
 var import_fields = require("@keystone-6/core/fields");
-var import_fields_document = require("@keystone-6/fields-document");
 var lists = {
   User: (0, import_core.list)({
     // WARNING
@@ -49,123 +48,33 @@ var lists = {
         isIndexed: "unique"
       }),
       password: (0, import_fields.password)({ validation: { isRequired: true } }),
-      // we can use this field to see what Posts this User has authored
-      //   more on that in the Post list below
-      posts: (0, import_fields.relationship)({ ref: "Post.author", many: true }),
       createdAt: (0, import_fields.timestamp)({
         // this sets the timestamp to Date.now() when the user is first created
         defaultValue: { kind: "now" }
       })
     }
   }),
-  Post: (0, import_core.list)({
-    // WARNING
-    //   for this starter project, anyone can create, query, update and delete anything
-    //   if you want to prevent random people on the internet from accessing your data,
-    //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
+  Course: (0, import_core.list)({
     access: import_access.allowAll,
-    // this is the fields for our Post list
     fields: {
       title: (0, import_fields.text)({ validation: { isRequired: true } }),
-      // the document field can be used for making rich editable content
-      //   you can find out more at https://keystonejs.com/docs/guides/document-fields
-      content: (0, import_fields_document.document)({
-        formatting: true,
-        layouts: [
-          [1, 1],
-          [1, 1, 1],
-          [2, 1],
-          [1, 2],
-          [1, 2, 1]
-        ],
-        links: true,
-        dividers: true
-      }),
-      // with this field, you can set a User as the author for a Post
-      author: (0, import_fields.relationship)({
-        // we could have used 'User', but then the relationship would only be 1-way
-        ref: "User.posts",
-        // this is some customisations for changing how this will look in the AdminUI
-        ui: {
-          displayMode: "cards",
-          cardFields: ["name", "email"],
-          inlineEdit: { fields: ["name", "email"] },
-          linkToItem: true,
-          inlineConnect: true
-        },
-        // a Post can only have one author
-        //   this is the default, but we show it here for verbosity
-        many: false
-      }),
-      // with this field, you can add some Tags to Posts
-      tags: (0, import_fields.relationship)({
-        // we could have used 'Tag', but then the relationship would only be 1-way
-        ref: "Tag.posts",
-        // a Post can have many Tags, not just one
+      units: (0, import_fields.relationship)({
+        ref: "Unit",
         many: true,
-        // this is some customisations for changing how this will look in the AdminUI
         ui: {
           displayMode: "cards",
-          cardFields: ["name"],
-          inlineEdit: { fields: ["name"] },
-          linkToItem: true,
-          inlineConnect: true,
-          inlineCreate: { fields: ["name"] }
+          cardFields: ["title", "order"],
+          linkToItem: true
         }
       })
     }
   }),
-  // this last list is our Tag list, it only has a name field for now
-  Tag: (0, import_core.list)({
-    // WARNING
-    //   for this starter project, anyone can create, query, update and delete anything
-    //   if you want to prevent random people on the internet from accessing your data,
-    //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
-    access: import_access.allowAll,
-    // setting this to isHidden for the user interface prevents this list being visible in the Admin UI
-    ui: {
-      isHidden: true
-    },
-    // this is the fields for our Tag list
-    fields: {
-      name: (0, import_fields.text)(),
-      // this can be helpful to find out all the Posts associated with a Tag
-      posts: (0, import_fields.relationship)({ ref: "Post.tags", many: true })
-    }
-  }),
-  Activity: (0, import_core.list)({
+  Unit: (0, import_core.list)({
     access: import_access.allowAll,
     fields: {
       title: (0, import_fields.text)({ validation: { isRequired: true } }),
-      activityLessons: (0, import_fields.relationship)({
-        ref: "ActivityLesson.activity",
-        many: true
-      })
-    }
-  }),
-  ActivityLesson: (0, import_core.list)({
-    access: import_access.allowAll,
-    fields: {
-      activity: (0, import_fields.relationship)({ ref: "Activity.activityLessons" }),
-      lesson: (0, import_fields.relationship)({ ref: "Lesson.activityLessons" }),
       order: (0, import_fields.integer)({
-        isOrderable: true,
-        validation: { isRequired: true },
-        ui: { itemView: { fieldMode: "edit" } }
-      })
-    }
-  }),
-  Lesson: (0, import_core.list)({
-    access: import_access.allowAll,
-    fields: {
-      title: (0, import_fields.text)({ validation: { isRequired: true } }),
-      activityLessons: (0, import_fields.relationship)({
-        ref: "ActivityLesson.lesson",
-        many: true,
-        ui: {
-          description: "Test",
-          displayMode: "select"
-        }
+        validation: { isRequired: true }
       })
     }
   })
--- admin-ui/schema.ts
+++ admin-ui/schema.ts
@@ -49,10 +49,6 @@ export const lists: Lists = {
 
       password: password({ validation: { isRequired: true } }),
 
-      // we can use this field to see what Posts this User has authored
-      //   more on that in the Post list below
-      posts: relationship({ ref: "Post.author", many: true }),
-
       createdAt: timestamp({
         // this sets the timestamp to Date.now() when the user is first created
         defaultValue: { kind: "now" },
@@ -60,127 +56,28 @@ export const lists: Lists = {
     },
   }),
 
-  Post: list({
-    // WARNING
-    //   for this starter project, anyone can create, query, update and delete anything
-    //   if you want to prevent random people on the internet from accessing your data,
-    //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
+  Course: list({
     access: allowAll,
-    // this is the fields for our Post list
     fields: {
       title: text({ validation: { isRequired: true } }),
-
-      // the document field can be used for making rich editable content
-      //   you can find out more at https://keystonejs.com/docs/guides/document-fields
-      content: document({
-        formatting: true,
-        layouts: [
-          [1, 1],
-          [1, 1, 1],
-          [2, 1],
-          [1, 2],
-          [1, 2, 1],
-        ],
-        links: true,
-        dividers: true,
-      }),
-
-      // with this field, you can set a User as the author for a Post
-      author: relationship({
-        // we could have used 'User', but then the relationship would only be 1-way
-        ref: "User.posts",
-
-        // this is some customisations for changing how this will look in the AdminUI
-        ui: {
-          displayMode: "cards",
-          cardFields: ["name", "email"],
-          inlineEdit: { fields: ["name", "email"] },
-          linkToItem: true,
-          inlineConnect: true,
-        },
-
-        // a Post can only have one author
-        //   this is the default, but we show it here for verbosity
-        many: false,
-      }),
-
-      // with this field, you can add some Tags to Posts
-      tags: relationship({
-        // we could have used 'Tag', but then the relationship would only be 1-way
-        ref: "Tag.posts",
-
-        // a Post can have many Tags, not just one
+      units: relationship({
+        ref: "Unit",
         many: true,
-
-        // this is some customisations for changing how this will look in the AdminUI
         ui: {
           displayMode: "cards",
-          cardFields: ["name"],
-          inlineEdit: { fields: ["name"] },
+          cardFields: ["title", "order"],
           linkToItem: true,
-          inlineConnect: true,
-          inlineCreate: { fields: ["name"] },
         },
       }),
     },
   }),
 
-  // this last list is our Tag list, it only has a name field for now
-  Tag: list({
-    // WARNING
-    //   for this starter project, anyone can create, query, update and delete anything
-    //   if you want to prevent random people on the internet from accessing your data,
-    //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
-    access: allowAll,
-
-    // setting this to isHidden for the user interface prevents this list being visible in the Admin UI
-    ui: {
-      isHidden: true,
-    },
-
-    // this is the fields for our Tag list
-    fields: {
-      name: text(),
-      // this can be helpful to find out all the Posts associated with a Tag
-      posts: relationship({ ref: "Post.tags", many: true }),
-    },
-  }),
-
-  Activity: list({
+  Unit: list({
     access: allowAll,
     fields: {
       title: text({ validation: { isRequired: true } }),
-      activityLessons: relationship({
-        ref: "ActivityLesson.activity",
-        many: true,
-      }),
-    },
-  }),
-
-  ActivityLesson: list({
-    access: allowAll,
-    fields: {
-      activity: relationship({ ref: "Activity.activityLessons" }),
-      lesson: relationship({ ref: "Lesson.activityLessons" }),
       order: integer({
-        isOrderable: true,
         validation: { isRequired: true },
-        ui: { itemView: { fieldMode: "edit" } },
-      }),
-    },
-  }),
-
-  Lesson: list({
-    access: allowAll,
-    fields: {
-      title: text({ validation: { isRequired: true } }),
-      activityLessons: relationship({
-        ref: "ActivityLesson.lesson",
-        many: true,
-        ui: {
-          description: "Test",
-          displayMode: "select",
-        },
       }),
     },
   }),
