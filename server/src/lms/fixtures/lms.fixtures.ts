ee8ead97909790fb934bd9fc374a96ddaa077e97
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-08-06 22:37:12 +0530

    add factories for entities

--- server/src/lms/fixtures/lms.fixtures.ts
+++ server/src/lms/fixtures/lms.fixtures.ts
@@ -2,31 +2,10 @@ import { faker } from '@faker-js/faker';
 import { Factory } from 'fishery';
 import { ProficiencyLevel } from 'src/auth/entities/profile.entity';
 import { baseFactory } from 'src/common/fixtures/base.fixture';
+import { Activity, ActivityType } from 'src/lms/entities/activity.entity';
 import { Course } from 'src/lms/entities/course.entity';
+import { Lesson } from 'src/lms/entities/lesson.entity';
 import { Unit } from 'src/lms/entities/unit.entity';
-
-export const unitFactory = Factory.define<Unit>(({ params, sequence }) => {
-  //   onCreate(async (profile) => {
-  //     const profileRepo = await getRepository(Profile);
-  //     return profileRepo.save(
-  //       {
-  //         ...profile,
-  //         userId: profile.userId
-  //           ? profile.userId
-  //           : (await firebaseUserFactory.create()).uid,
-  //       },
-  //       { reload: true },
-  //     );
-  //   });
-
-  return {
-    ...baseFactory.build(params),
-    title: faker.lorem.sentence(5),
-    order: sequence,
-    course: courseFactory.build(),
-    lessons: [],
-  };
-});
 
 export const courseFactory = Factory.define<Course>(({ params }) => {
   //   onCreate(async (profile) => {
@@ -49,3 +28,79 @@ export const courseFactory = Factory.define<Course>(({ params }) => {
     units: [],
   };
 });
+
+export const unitFactory = Factory.define<Unit>(
+  ({ params, sequence, associations }) => {
+    //   onCreate(async (profile) => {
+    //     const profileRepo = await getRepository(Profile);
+    //     return profileRepo.save(
+    //       {
+    //         ...profile,
+    //         userId: profile.userId
+    //           ? profile.userId
+    //           : (await firebaseUserFactory.create()).uid,
+    //       },
+    //       { reload: true },
+    //     );
+    //   });
+
+    return {
+      ...baseFactory.build(params),
+      title: faker.lorem.sentence(5),
+      order: sequence,
+      course: associations.course || courseFactory.build(),
+      lessons: [],
+    };
+  },
+);
+
+export const lessonFactory = Factory.define<Lesson>(
+  ({ params, sequence, associations }) => {
+    //   onCreate(async (profile) => {
+    //     const profileRepo = await getRepository(Profile);
+    //     return profileRepo.save(
+    //       {
+    //         ...profile,
+    //         userId: profile.userId
+    //           ? profile.userId
+    //           : (await firebaseUserFactory.create()).uid,
+    //       },
+    //       { reload: true },
+    //     );
+    //   });
+
+    return {
+      ...baseFactory.build(params),
+      title: faker.lorem.sentence(5),
+      order: sequence,
+      unit: associations.unit || unitFactory.build(),
+      activities: [],
+    } as Lesson;
+  },
+);
+
+export const activityFactory = Factory.define<Activity>(
+  ({ params, sequence, associations }) => {
+    //   onCreate(async (profile) => {
+    //     const profileRepo = await getRepository(Profile);
+    //     return profileRepo.save(
+    //       {
+    //         ...profile,
+    //         userId: profile.userId
+    //           ? profile.userId
+    //           : (await firebaseUserFactory.create()).uid,
+    //       },
+    //       { reload: true },
+    //     );
+    //   });
+
+    return {
+      ...baseFactory.build(params),
+      title: faker.lorem.sentence(5),
+      order: sequence,
+      lesson: associations.lesson || lessonFactory.build(),
+      data: {},
+      type: ActivityType.FILL_IN_THE_BLANKS,
+    };
+  },
+);
