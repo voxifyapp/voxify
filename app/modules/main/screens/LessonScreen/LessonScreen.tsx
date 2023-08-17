c810df66772ae66168c9434c001b50a2d1069230
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-08-17 12:16:57 +0530

    get activities for lesson

--- app/modules/main/screens/LessonScreen/LessonScreen.tsx
+++ app/modules/main/screens/LessonScreen/LessonScreen.tsx
@@ -1,6 +1,8 @@
 import {
-  GET_LESSON_WITH_ACTIVITIES,
-  getLessonWithActivities,
+  GET_LESSON,
+  GET_LESSON_ACTIVITIES,
+  getLesson,
+  getLessonActivities,
 } from '@voxify/api/lms/lms';
 import React from 'react';
 import { useQuery } from 'react-query';
@@ -13,14 +15,20 @@ type Props = {
 export const LessonScreen = ({
   lessonId = 'fdfb262e-cf99-4c31-84ed-574bb3f53241',
 }: Props) => {
-  const { data: lessonData, isLoading } = useQuery({
-    queryFn: getLessonWithActivities.bind(null, lessonId),
-    queryKey: [GET_LESSON_WITH_ACTIVITIES, lessonId],
+  const { isLoading: isLessonLoading } = useQuery({
+    queryFn: getLesson.bind(null, lessonId),
+    queryKey: [GET_LESSON, lessonId],
   });
 
-  console.log(lessonData);
+  const { data: lessonActivities, isLoading: isLessonActivitiesLoading } =
+    useQuery({
+      queryFn: getLessonActivities.bind(null, lessonId),
+      queryKey: [GET_LESSON_ACTIVITIES, lessonId],
+    });
 
-  if (isLoading) {
+  console.log(lessonActivities);
+
+  if (isLessonLoading || isLessonActivitiesLoading) {
     return <H1>Loading...</H1>;
   }
 
