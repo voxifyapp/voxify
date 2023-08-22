80df4579d6b15660bf22eba8e3d30b4a24a1b4a3
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-08-22 15:41:59 +0530

    Create and write test for controller

--- api/src/lms-progress/controllers/activity-response.controller.spec.ts
+++ api/src/lms-progress/controllers/activity-response.controller.spec.ts
@@ -1,18 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ActivityResponseController } from './activity-response.controller';
+import { ActivityResponseService } from 'src/lms-progress/services/activity-response.service';
+import { CreateActivityResponseDto } from 'src/lms-progress/dtos/create-activity-response.dto';
+import { AuthenticatedRequestWithProfile } from 'src/common/request';
+import { activityResponseFactory } from 'src/lms-progress/fixtures/activity-response.fixture';
+import { activityFactory } from 'src/lms/fixtures/lms.fixtures';
+import { mockService } from 'src/common/mocks/mockService';
 
 describe('ActivityResponseController', () => {
   let controller: ActivityResponseController;
+  let service: ActivityResponseService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ActivityResponseController],
+      providers: [mockService(ActivityResponseService)],
     }).compile();
 
-    controller = module.get<ActivityResponseController>(ActivityResponseController);
+    controller = module.get<ActivityResponseController>(
+      ActivityResponseController,
+    );
+    service = module.get<ActivityResponseService>(ActivityResponseService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('create', () => {
+    it('should create a new activity response', async () => {
+      const req: AuthenticatedRequestWithProfile = {
+        currentProfile: { id: 'profile-id' },
+      } as any;
+
+      const expectedResult = activityResponseFactory.build({
+        id: 'arid',
+        activity: activityFactory.build({ id: 'activity-id' }),
+      });
+      service.create = jest.fn().mockResolvedValue(expectedResult);
+
+      const result = await controller.create(req, {
+        activityId: 'activity-id',
+        responseData: {},
+        timeTaken: 10,
+        result: 'SUCCESS',
+      } as CreateActivityResponseDto);
+
+      expect(result).toBe(expectedResult);
+      expect(service.create).toHaveBeenCalledWith('profile-id', {
+        activityId: 'activity-id',
+        responseData: {},
+        timeTaken: 10,
+        result: 'SUCCESS',
+      });
+    });
   });
 });
---
+++ api/src/lms-progress/fixtures/activity-response.fixture.ts
@@ -0,0 +1,34 @@
+import { faker } from '@faker-js/faker';
+import { Factory } from 'fishery';
+import { baseFactory } from 'src/common/fixtures/base.fixture';
+import {
+  ActivityResponse,
+  ResultType,
+} from 'src/lms-progress/entities/activity-response.entity';
+
+export const activityResponseFactory = Factory.define<ActivityResponse>(
+  ({ params }) => {
+    // onCreate(async (activity) => {
+    //   const activityRepo = await getRepository(Activity);
+    //   return activityRepo.save(
+    //     {
+    //       ...omit(activity, []),
+    //       lesson:
+    //         activity.lesson === undefined
+    //           ? await lessonFactory.create()
+    //           : activity.lesson,
+    //     },
+    //     { reload: true },
+    //   );
+    // });
+
+    return {
+      ...baseFactory.build(params),
+      activity: undefined,
+      result: ResultType.SUCCESS,
+      responseData: {},
+      timeTaken: faker.number.float({ max: 100, min: 0 }),
+      profile: undefined,
+    };
+  },
+);
