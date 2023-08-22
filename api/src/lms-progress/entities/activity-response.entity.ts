411c4d79a5b0c139dca2aab44f7aa53f24e4ce1e
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-08-22 10:49:06 +0530

    Create activity response model

---
+++ api/src/lms-progress/entities/activity-response.entity.ts
@@ -0,0 +1,35 @@
+import { BaseEntity } from 'src/common/entities/base.entity';
+import { Activity } from 'src/lms/entities/activity.entity';
+import { Entity, Column, ManyToOne } from 'typeorm';
+
+export enum ResultType {
+  SUCCESS = 'SUCCESS',
+  FAIL = 'FAIL',
+}
+
+@Entity()
+export class ActivityResponse extends BaseEntity {
+  @Column({
+    comment:
+      'The data the user submitted for the activity. We will use this to track how they interacted with each activity',
+    type: 'jsonb',
+    default: {},
+  })
+  responseData: object;
+
+  @Column({
+    comment: 'The number of seconds the user spent on this activity',
+    type: 'real',
+  })
+  timeTaken: number;
+
+  @Column({
+    comment: 'Did the user pass or fail this activity?',
+  })
+  result: ResultType;
+
+  @ManyToOne(() => Activity, {
+    onDelete: 'CASCADE',
+  })
+  activity: Activity;
+}
