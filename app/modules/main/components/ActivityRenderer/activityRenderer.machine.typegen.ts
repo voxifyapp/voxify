cd575d63308da49b17168d196d9b5e19207d877d
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-09-05 13:55:46 +0530

    dsiable typegen eslint

--- app/modules/main/components/ActivityRenderer/activityRenderer.machine.typegen.ts
+++ app/modules/main/components/ActivityRenderer/activityRenderer.machine.typegen.ts
@@ -1,46 +1,39 @@
-// This file was automatically generated. Edits will be overwritten
 
-export interface Typegen0 {
-  '@@xstate/typegen': true;
-  internalEvents: {
-    'xstate.init': { type: 'xstate.init' };
-    'xstate.stop': { type: 'xstate.stop' };
-  };
-  invokeSrcNameMap: {};
-  missingImplementations: {
-    actions: never;
-    delays: never;
-    guards: never;
-    services: never;
-  };
-  eventsCausingActions: {
-    pauseTimer: 'UNFOCUSED' | 'finish' | 'xstate.stop';
-    restoreData: 'RESTORE_DATA';
-    setResult: 'set_result';
-    setUserAnswer: 'finish';
-    startTimer: 'FOCUSED';
-  };
-  eventsCausingDelays: {};
-  eventsCausingGuards: {};
-  eventsCausingServices: {};
-  matchesStates:
-    | 'FOCUSED_STATE'
-    | 'FOCUSED_STATE.FOCUSED'
-    | 'FOCUSED_STATE.UNFOCUSED'
-    | 'WORKING_STATE'
-    | 'WORKING_STATE.FINISHED'
-    | 'WORKING_STATE.NOT_STARTED'
-    | 'WORKING_STATE.PAUSED'
-    | 'WORKING_STATE.RESULT'
-    | 'WORKING_STATE.WORKING'
-    | {
-        FOCUSED_STATE?: 'FOCUSED' | 'UNFOCUSED';
-        WORKING_STATE?:
-          | 'FINISHED'
-          | 'NOT_STARTED'
-          | 'PAUSED'
-          | 'RESULT'
-          | 'WORKING';
-      };
-  tags: never;
-}
+  // This file was automatically generated. Edits will be overwritten
+
+  export interface Typegen0 {
+        '@@xstate/typegen': true;
+        internalEvents: {
+          "xstate.init": { type: "xstate.init" };
+"xstate.stop": { type: "xstate.stop" };
+        };
+        invokeSrcNameMap: {
+          
+        };
+        missingImplementations: {
+          actions: never;
+          delays: never;
+          guards: never;
+          services: never;
+        };
+        eventsCausingActions: {
+          "pauseTimer": "UNFOCUSED" | "finish" | "xstate.stop";
+"restoreData": "RESTORE_DATA";
+"setResult": "set_result";
+"setUserAnswer": "finish";
+"startTimer": "FOCUSED";
+        };
+        eventsCausingDelays: {
+          
+        };
+        eventsCausingGuards: {
+          
+        };
+        eventsCausingServices: {
+          
+        };
+        matchesStates: "FOCUSED_STATE" | "FOCUSED_STATE.FOCUSED" | "FOCUSED_STATE.UNFOCUSED" | "WORKING_STATE" | "WORKING_STATE.FINISHED" | "WORKING_STATE.NOT_STARTED" | "WORKING_STATE.PAUSED" | "WORKING_STATE.RESULT" | "WORKING_STATE.WORKING" | { "FOCUSED_STATE"?: "FOCUSED" | "UNFOCUSED";
+"WORKING_STATE"?: "FINISHED" | "NOT_STARTED" | "PAUSED" | "RESULT" | "WORKING"; };
+        tags: never;
+      }
+  
