95d2e5dbf835400a81b2089fb14d6ea5d7b292d9
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-09-18 11:01:00 +0530

    Make common modules work

--- app/metro.config.js
+++ app/metro.config.js
@@ -2,11 +2,14 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const path = require('path');
 
 /**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    unstable_enableSymlinks: true,
+    nodeModulesPaths: [path.resolve(__dirname, 'node_modules')],
+  },
+  watchFolders: [path.resolve(__dirname, '../packages/activity-builder')],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
