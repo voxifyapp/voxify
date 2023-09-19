3a259d08a425f735fd474239ee0b1b18ef11c575
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-09-19 10:40:51 +0530

    Use new video format in app

--- app/appConstants.tsx
+++ app/appConstants.tsx
@@ -2,4 +2,5 @@ import Config from 'react-native-config';
 
 export class Constants {
   static readonly VOXIFY_API_ENDPOINT = Config.VOXIFY_API_ENDPOINT;
+  static readonly ACTIVITY_VIDEO_URL = Config.ACTIVITY_VIDEO_URL;
 }
--- app/modules/main/screens/LessonScreen/components/Video.tsx
+++ app/modules/main/screens/LessonScreen/components/Video.tsx
@@ -1,4 +1,5 @@
 import { VideoActivity } from '@packages/activity-builder';
+import { Constants } from '@voxify/appConstants';
 import React from 'react';
 import RNVideo from 'react-native-video';
 import { YStack } from 'tamagui';
@@ -17,9 +18,13 @@ export const Video = ({ activity }: Props) => {
         onBuffer={data => console.log(data)}
         reportBandwidth={true}
         onPlaybackStalled={() => console.log('Stalled')}
-        source={{ uri: activity.getVideoUrl() }}
+        source={{ uri: getVideoUrlFromFileName(activity.getVideoFileName()) }}
         style={{ width: '100%', height: '100%', backgroundColor: 'black' }}
       />
     </YStack>
   );
+};
+
+export const getVideoUrlFromFileName = (fileName: string): string => {
+  return `${Constants.ACTIVITY_VIDEO_URL}/${fileName}.mp4`;
 };
