5b779a63030525e9a528573df0abd93140fa138f
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-10-03 20:44:05 +0530

    Fix erros

--- admin/src/app/dashboard/page.tsx
+++ admin/src/app/dashboard/page.tsx
@@ -1,8 +1,3 @@
-import { nextAuthOptions } from '@/app/api/auth/[...nextauth]/route';
-import { getServerSession } from 'next-auth/next';
-import { redirect } from 'next/navigation';
-import { headers, cookies } from 'next/headers';
-import { getToken } from 'next-auth/jwt';
 import { getFirebaseTokenForRequest } from '@/lib/firebaseAdmin';
 
 export default async function Dashboard() {
