136da21e1b62defd37086d8b144bd2995b2ccc40
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-11-09 15:13:37 +0530

    hide unpublished content on activities

--- admin/src/app/dashboard/activities/page.tsx
+++ admin/src/app/dashboard/activities/page.tsx
@@ -6,7 +6,9 @@ import { Activity, Lesson } from '@/types/lms';
 import {
   Box,
   Button,
+  Checkbox,
   CircularProgress,
+  FormControlLabel,
   Paper,
   Stack,
   Table,
@@ -19,11 +21,14 @@ import {
 import dayjs from 'dayjs';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 
 type ActivityWithLesson = Activity & { lesson?: Lesson };
 
 export default function Units() {
+  const [hideUnpublishedContent, setHideUnpublishedContent] = useState(true);
+
   const searchParams = useSearchParams();
   const lessonId = searchParams.get('lessonId');
 
@@ -35,16 +40,32 @@ export default function Units() {
       }),
   });
 
+  const filteredActivities = activities?.filter(activity =>
+    hideUnpublishedContent ? activity.published === true : true,
+  );
+
   return (
     <Box padding={2}>
       <TableContainer component={Paper}>
-        <Link
-          href={{
-            pathname: '/dashboard/activities/create-edit',
-            query: { lessonId },
-          }}>
-          <Button>Create Activity</Button>
-        </Link>
+        <Box padding={2} display="flex" flexDirection="row">
+          <Link
+            href={{
+              pathname: '/dashboard/activities/create-edit',
+              query: { lessonId },
+            }}>
+            <Button>Create Activity</Button>
+          </Link>
+          <Box flex={1} />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={hideUnpublishedContent}
+                onChange={e => setHideUnpublishedContent(e.target.checked)}
+              />
+            }
+            label="Hide unpublished content"
+          />
+        </Box>
         {isLoading && <CircularProgress />}
         {lessonId && <h3>For Lesson: {lessonId}</h3>}
         <Table>
@@ -59,8 +80,8 @@ export default function Units() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {activities &&
-              activities.map(activity => (
+            {filteredActivities &&
+              filteredActivities.map(activity => (
                 <ActivityRow key={activity.id} activity={activity} />
               ))}
           </TableBody>
