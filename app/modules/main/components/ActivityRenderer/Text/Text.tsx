bcd7f9c762df3ae8f91463bbf9208b00f0035f5f
Faiz Ahmed <faizsameerahmed96@gmail.com>
2023-11-26 14:38:16 +0530

    Reddesign text activity

--- app/modules/main/components/ActivityRenderer/Text/Text.tsx
+++ app/modules/main/components/ActivityRenderer/Text/Text.tsx
@@ -1,18 +1,23 @@
 import { TextActivity } from '@packages/activity-builder';
+import { Button } from '@voxify/design_system/button';
+import { H2, Paragraph } from '@voxify/design_system/typography';
 import { useActivityRendererContext } from '@voxify/modules/main/components/ActivityRenderer/activityRenderer.context';
+import { ActivityCardContainer } from '@voxify/modules/main/components/ActivityRenderer/common/ActivityCardContainer';
 
 import { ActivityResponseResultType } from '@voxify/types/lms-progress/activity-response';
 import React from 'react';
-import { Button, Card, H1, H3, H5, Stack, YStack } from 'tamagui';
+import { YStack } from 'tamagui';
 
 type Props = {
   activity: TextActivity;
   heading?: string;
 };
 
-export const Text = ({ activity, heading }: Props) => {
+export const Text = ({ activity }: Props) => {
   const { machineService: activityRendererMachineService } =
     useActivityRendererContext();
+
+  console.log(activity);
 
   const onDoneClicked = () => {
     activityRendererMachineService.send({ type: 'finish', userAnswer: {} });
@@ -26,20 +31,18 @@ export const Text = ({ activity, heading }: Props) => {
   };
 
   return (
-    <YStack padding="$3" fullscreen>
-      {heading && <H3>{heading}</H3>}
-      <Card size="$5" width={250} height={300}>
-        <Card.Header padded>
-          <H3>{activity.getTitle().text}</H3>
-          <H5>{activity.getDescription().text}</H5>
-        </Card.Header>
-      </Card>
-      <Stack flex={1} />
+    <ActivityCardContainer>
+      <YStack flex={1} alignItems="center" justifyContent="center">
+        <H2 textAlign="center">{activity.getTitle().text}</H2>
+        <Paragraph mt="$2" textAlign="center">
+          {activity.getDescription().text}
+        </Paragraph>
+      </YStack>
       {activityRendererMachineService
         .getSnapshot()
         ?.can({ type: 'finish', userAnswer: {} }) && (
         <Button onPress={onDoneClicked}>Done</Button>
       )}
-    </YStack>
+    </ActivityCardContainer>
   );
 };
--- packages/activity-builder/dist/text-activity.js
+++ packages/activity-builder/dist/text-activity.js
@@ -6,7 +6,7 @@ const text_block_1 = require("./blocks/text-block");
 class TextActivity extends activity_1.Activity {
     constructor(data) {
         super(activity_1.ActivityType.TEXT, data
-            ? Object.assign(Object.assign({}, data), { title: new text_block_1.TextBlock(data.title.text, data.title), description: new text_block_1.TextBlock(data.title.text, data.title) }) : { title: new text_block_1.TextBlock(""), description: new text_block_1.TextBlock("") });
+            ? Object.assign(Object.assign({}, data), { title: new text_block_1.TextBlock(data.title.text, data.title), description: new text_block_1.TextBlock(data.description.text, data.description) }) : { title: new text_block_1.TextBlock(""), description: new text_block_1.TextBlock("") });
     }
     setTitle(title) {
         this.setData(Object.assign(Object.assign({}, this.getData()), { title }));
